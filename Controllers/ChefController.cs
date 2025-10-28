e78976ae67ee79e796bc037fa0b33b0eb51ffb6e
nhattoan <nguyennhattoan74@gmail.com>
2025-10-28 16:46:08 +0700

    Initial commit

---
+++ Controllers/ChefController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DatBan.Controllers
+{
+    public class ChefController : Controller
+    {
+        // GET: Chef
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
